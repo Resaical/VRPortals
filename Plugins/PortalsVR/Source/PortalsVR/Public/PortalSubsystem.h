9c06c32e8b9c51a6575eaa5ac45bce19efce0e3d
Resaical <javimoreno94@hotmail.com>
2025-09-29 18:54:30 +0200

    [ADD] Added stereoscopic rendering via FSceneViewExtension
    - [ADD] PortalSubsystem to manage link between portals and FSceneViewExtension
    - [ADD] PortalViewExtension to process the headset input and create the stereoscopic effect using this tool. ( end game_threat - beginning render_threat.
    - [PROBLEM] This pipeline renders 1 frame late, solving this forces me to abandone this method and moving to other strategy.

--- Plugins/PortalsVR/Source/PortalsVR/Public/PortalSubsystem.h
+++ Plugins/PortalsVR/Source/PortalsVR/Public/PortalSubsystem.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include <PortalViewExtension.h>
-#include <Portal.h>
-#include <PortalVR.h>
+#include "Subsystems/WorldSubsystem.h"
+#include "PortalViewExtension.h"
+#include "Portal.h"
+#include "PortalVR.h"
 #include "PortalSubsystem.generated.h"
 
 UCLASS()
-
-class UPortalSubsystem : public UWorldSubsystem
+class UPortalSubsystem : public UTickableWorldSubsystem
 {
     GENERATED_BODY()
 
@@ -16,15 +16,33 @@ public:
     virtual void Initialize(FSubsystemCollectionBase& Collection) override
     {
         Super::Initialize(Collection);
-        ViewExtension = FSceneViewExtensions::NewExtension<FPortalViewExtension>();
+
+        //ViewExtension = FSceneViewExtensions::NewExtension<FPortalViewExtension>();
     }
 
     virtual void Deinitialize() override
     {
-        ViewExtension.Reset(); // unregister extension
+        //ViewExtension.Reset();
+
         Super::Deinitialize();
-    }    
+    }
+
+    virtual TStatId GetStatId() const override
+    {
+        RETURN_QUICK_DECLARE_CYCLE_STAT(UPortalSubsystem, STATGROUP_Tickables);
+    }
+
+    virtual void Tick(float DeltaTime) override
+    {
+        Super::Tick(DeltaTime);
+
+        leftRendered = false;
+        rightRendered = false;
+    }
 
     TSharedPtr<FPortalViewExtension, ESPMode::ThreadSafe> ViewExtension;
     TArray<TWeakObjectPtr<APortalVR>> ActivePortals;
+
+    bool leftRendered = false;
+    bool rightRendered = false;
 };
